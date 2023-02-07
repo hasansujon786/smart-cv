e6a04ef6db6d4edd29e273fa047ba5829e18cab4
Hasan Mahmud <hasansujon786@gmail.com>
2023-02-07 16:40:16 +0600

    add all RootNavigator

--- composables/useLazyScreenLoader.js
+++ composables/useLazyScreenLoader.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useState, } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box } from 'native-base'
 import { themeColors } from '../constant/globalStyles'
 
 export const useLazyScreenLoader = () => {
   const [isPageReady, setIsPageReady] = useState(false)
 
-  useEffect(() => setIsPageReady(true), [])
+  // TODO: <07.02.23> try to get someting better than this hack
+  useEffect(() => {
+    setTimeout(() => {
+      setIsPageReady(true)
+    }, 0)
+  }, [])
 
   return { isPageReady }
 }
 
-
 export const LazyScreenLoader = () => {
   return <Box flex={1} _light={{ bg: themeColors.light.bgDark }} _dark={{ bg: themeColors.dark.bgDark }} />
 }
-
--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,22 +1,63 @@
+import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import GradientHeader from '../components/GradientHeader'
 
 import HomeNavigator from './HomeNavigator'
+import SelectTemplateNavigator from './SelectTemplateNavigator'
+
 import Profiles from '../screen/Profiles'
+import Template from '../screen/Template'
+import CreateProfile from '../screen/CreateProfile/CreateProfile'
+import PersonalDetails from '../screen/CreateProfile/PersonalDetails'
+import Education from '../screen/CreateProfile/Education'
+import Experience from '../screen/CreateProfile/Experience'
+import Objective from '../screen/CreateProfile/Objective'
+import Skills from '../screen/CreateProfile/Skills'
+import Achievements from '../screen/CreateProfile/Achievements'
+import Interests from '../screen/CreateProfile/Interests'
+import Language from '../screen/CreateProfile/Language'
+import AdditionalInformation from '../screen/CreateProfile/AdditionalInformation'
+import References from '../screen/CreateProfile/References'
+import Projects from '../screen/CreateProfile/Projects'
+import ZoomView from '../screen/ZoomView'
 
 const Stack = createStackNavigator()
 
-function RootNavigation() {
+const RootNavigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        header: (props) => <GradientHeader {...props} />,
+        header: props => <GradientHeader {...props} />
       }}
     >
       <Stack.Screen name='Home' component={HomeNavigator} options={{ title: 'Smart CV', headerShown: false }} />
+
       <Stack.Screen name='Profiles' component={Profiles} options={{ title: 'Choose Profile' }} />
+      <Stack.Screen name='CreateProfile' component={CreateProfile} options={({ route }) => ({ title: route.params.title || 'Create Profile' })} />
+      <Stack.Screen name='PersonalDetails' component={PersonalDetails} options={{ title: 'Personal Details', }} />
+      <Stack.Screen name='Education' component={Education} />
+      <Stack.Screen name='Experience' component={Experience} />
+      <Stack.Screen name='Objective' component={Objective} />
+      <Stack.Screen name='Skills' component={Skills} />
+      <Stack.Screen name='Interests' component={Interests} />
+      <Stack.Screen name='Achievements' component={Achievements} />
+      <Stack.Screen name='Language' component={Language} />
+      <Stack.Screen name='AdditionalInformation' component={AdditionalInformation} options={{ title: 'Additional Info' }} />
+      <Stack.Screen name='References' component={References} />
+      <Stack.Screen name='Projects' component={Projects} />
+
+      <Stack.Screen name='SelectTemplate' component={SelectTemplateNavigator} options={{ title: 'Select Template' }} />
+      <Stack.Screen name='Template' component={Template} />
+      <Stack.Screen name='ZoomView' component={ZoomView}
+        options={{
+          title: 'Smart CV',
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: '#49494a' },
+          headerTintColor: '#aaa',
+        }}
+      />
     </Stack.Navigator>
   )
 }
 
-export default RootNavigation
+export default RootNavigator
