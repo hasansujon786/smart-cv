a9769c96ca23bef796c4ef7f43693542af527134
Hasan Mahmud <hasansujon786@gmail.com>
2023-05-14 16:43:41 +0600

    perf(base): replace native-base in ThemeSelectorBottomBar

--- components/ThemeSelectorBottomBar.js
+++ components/ThemeSelectorBottomBar.js
@@ -1,76 +1,51 @@
 import React, { useState } from 'react'
-import { TouchableOpacity } from 'react-native'
-import { HStack, Center, Image, useColorModeValue, Box } from 'native-base'
-import { themeColors } from '../constant/globalStyles'
-
-const Loaderbar = () => {
-  return (
-    <HStack
-      px={8}
-      justifyContent='space-between'
-      position='absolute'
-      _dark={{ bg: 'gray.500' }}
-      bg='white'
-      top={0}
-      bottom={0}
-      right={0}
-      left={0}
-      opacity={0.8}
-    />
-  )
-}
+import { Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { Stack, XStack } from 'tamagui'
 
 const ThemeSelectorBottomBar = ({ onSelect, themes, onShowPicker, isLoading }) => {
   const [selectedTheme, setSelectedTheme] = useState(0)
-  const notSelectedBorderColor = useColorModeValue('gray.300', 'gray.600')
+
+  const handlePress = (idx) => {
+    if (isLoading || selectedTheme == idx) return
+    onSelect(idx)
+    setSelectedTheme(idx)
+  }
 
   return (
-    <Box position='relative'>
-      <HStack
-        space={2}
-        py={2}
-        px={6}
-        justifyContent='space-between'
-        _light={{ bg: themeColors.light.bg }}
-        _dark={{ bg: themeColors.dark.bg }}
-      >
-        {themes.map((theme, idx) => (
-          <TouchableOpacity
-            key={theme}
-            onPress={() => {
-              if (isLoading || selectedTheme == idx) return
-              onSelect(idx)
-              setSelectedTheme(idx)
-            }}
-          >
-            <Center
-              p={2}
-              width={8}
-              height={8}
-              bg={theme}
-              borderRadius={20}
-              borderWidth={1}
-              borderColor={notSelectedBorderColor}
-            >
-              {selectedTheme == idx && <Box opacity={0.5} bg='gray.100' width='100%' height='100%' borderRadius={20} />}
-            </Center>
-          </TouchableOpacity>
-        ))}
-
+    <XStack bg='$layer1' py='$2' px='$4' justifyContent='space-between'>
+      {themes.map((theme, idx) => (
         <TouchableOpacity
-          onPress={() => {
-            if (isLoading) return
-            onShowPicker()
-          }}
+          key={theme}
+          onPress={() => handlePress(idx)}
+          disabled={isLoading}
+          style={[styles.btn, { backgroundColor: theme }]}
         >
-          <Center width={8} height={8}>
-            <Image width='100%' height='100%' source={require('../assets/paint.png')} alt='theme picker icon' />
-          </Center>
+          {selectedTheme == idx && <Stack opacity={0.5} bg='black' width={15} height={15} borderRadius={20} />}
         </TouchableOpacity>
-      </HStack>
-      {isLoading && <Loaderbar />}
-    </Box>
+      ))}
+
+      <TouchableOpacity onPress={onShowPicker} disabled={isLoading} style={[styles.btn, { borderWidth: 0 }]}>
+        <Image
+          source={require('../assets/paint.png')}
+          alt='theme picker icon'
+          style={{ width: '100%', height: '100%' }}
+        />
+      </TouchableOpacity>
+
+      {isLoading && <Stack position='absolute' bg='$layer1' top={0} bottom={0} right={0} left={0} opacity={0.8} />}
+    </XStack>
   )
 }
 
 export default ThemeSelectorBottomBar
+
+const styles = StyleSheet.create({
+  btn: {
+    borderRadius: 22,
+    height: 32,
+    width: 32,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
